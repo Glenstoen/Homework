bf01485658704948581c514ad53718417b0fc0f4
Glenstoen <gln.stoen@gmail.com>
2023-09-28 10:50:47 +0200

    dunno

--- Recursive-functions/recursive_functions/recurse.py
+++ Recursive-functions/recursive_functions/recurse.py
@@ -2,8 +2,8 @@ def recurse_fac(x):
     if x == 1:
         return 1
     else:
-        return (x * recurse_fac(x-1))
+        return (x * recurse_fac(x - 1))
     
 num = int(input("enter a number\n"))
 result = recurse_fac(num)
-print(f"the factorial of {num} is {result}")
+print(f"the factorial of {num} is {result}") 
